a312e22c3a2964994361e71773fe16272298116f
周昊 <zhouhao1@corp.netease.com>
2018-11-15 14:53:28 +0800

    feat: demo24, demo25, demo26

---
+++ src/components/demos/demo25.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>富文本编辑器</h1>
+    <div class="inner-content" contenteditable="true" @mouseup="input"></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'demo25',
+  props: [],
+  data() {
+    return {
+      value: '',
+    }
+  },
+  mounted() {
+  },
+  // 获取选区
+  methods: {
+    input(e) {
+      if(document.getSelection().toString()) {
+        console.log(document.getSelection().toString())
+      }
+    }
+  },
+  computed: {}
+}
+</script>
+<style scoped>
+.inner-content {
+  width: 500px;
+  height: 500px;
+  margin: 20px auto;
+  padding: 20px;
+  text-align: left;
+  border: 1px solid darkcyan;
+}
+</style>
