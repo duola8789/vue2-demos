1c380210c6a80f40104969f11a647a69d44f4acc
zh <duola8789@126.com>
2019-09-18 19:08:36 +0800

    feat: 路由懒加载区分环境

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,22 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 // 懒加载工厂函数
-const lazyLoad = path => () => import(/* webpackChunkName: "view-[request]-[index]" */ `@/pages/${path}.vue`);
+const lazyLoad = path => {
+  if (process.env.NODE_ENV === 'development') {
+    return require(`@/pages/${path}.vue`).default
+  }
+  return () => import(/* webpackChunkName: "view-[request]-[index]" */ `@/pages/${path}.vue`);
+};
 
 const routes = [
-    {path: '/', name: 'home', component: lazyLoad('Home'), alias: '/home'},
-    {path: '/user', name: 'user', component: lazyLoad('User')},
-    {path: '/about', name: 'about', component: lazyLoad('About')},
-    {path: '*', name: 'notFound', component: lazyLoad('NotFound')},
+  {path: '/', name: 'home', component: lazyLoad('Home'), alias: '/home'},
+  {path: '/user', name: 'user', component: lazyLoad('User')},
+  {path: '/about', name: 'about', component: lazyLoad('About')},
+  {path: '*', name: 'notFound', component: lazyLoad('NotFound')},
 ];
 
 Vue.use(Router);
 
 export default new Router({
-    routes,
+  routes,
 });
