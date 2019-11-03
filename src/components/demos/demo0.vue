a4cd824c2005c0257ac4e6043d3aa211c99ea816
zh <duola8789@126.com>
2019-11-03 11:32:31 +0800

    feat: demo0

--- src/components/demos/demo0.vue
+++ src/components/demos/demo0.vue
@@ -1,27 +1,46 @@
 <template>
   <div class="main">
-    <h1>空的Demo，随便干什么</h1>
-
+    <p>结果：{{message}}</p>
+    <button @click="clickHandler(1)">1</button>
+    <button @click="clickHandler(2)">2</button>
+    <button @click="clickHandler(3)">3</button>
   </div>
 </template>
 
 <script>
+
+const request = () => {
+  let a = 1;
+  return () => {
+    return a++;
+  }
+};
+
+
 export default {
   name: 'demo0',
   data() {
     return {
       message: '',
+      selected: 0,
     }
   },
 
-  mounted() {
-    fetch('https://xy.com:8888/').then(v => v.json()).then(v => { this.message = v.message })
-  },
-
-  computed: {
-  },
-
   methods: {
+    async clickHandler(type) {
+      this.selected = type;
+      const res = await this.fetch(type);
+      if (type !== this.selected) {
+        return;
+      }
+      this.message = res;
+    },
+
+    fetch(time) {
+      return new Promise(resolve => {
+        setTimeout(resolve, time * 1000, time)
+      })
+    }
   },
 }
 </script>
@@ -30,5 +49,12 @@ export default {
   .main {
     font-size: 16px;
     color: darkcyan;
+  }
+  button {
+    cursor: pointer;
+    transition: all 0.1s;
+  }
+  button:focus {
+    background: darkcyan;
   }
 </style>
