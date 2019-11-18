39c4348ddf2c75ec3c97d0832019cab88149ea01
zh <duola8789@126.com>
2019-11-18 09:57:07 +0800

    feat: demo40-可拖动表格

--- src/components/demos/demo0.vue
+++ src/components/demos/demo0.vue
@@ -1,94 +1,14 @@
 <template>
   <div class="main">
 
-    <el-table :data="tableData">
-
-
-      <el-table-column width="55">
-        <template slot-scope="scope">
-          <el-button v-if="!['start', 'end'].includes(scope.row.rowId)"
-                     :class="handlerSelector"
-                     icon="el-icon-rank"
-                     size="small"
-                     type="text">
-          </el-button>
-        </template>
-      </el-table-column>
-
-      <el-table-column type="index" width="100" :index="indexMethod"></el-table-column>
-
-      <el-table-column v-for="column in columns"
-                       :key="column.prop"
-                       :prop="column.prop"
-                       :label="column.label"
-                       :min-width="column.minWidth">
-        <template slot-scope="scope">
-          <template v-if="scope.row.isEditing">
-            <!--
-            <el-form ref="form" :model="editingRow" label-width="80px">
-              <el-form-item>
-                <el-input v-model="editingRow[column.prop]"></el-input>
-              </el-form-item>
-            </el-form>
-            -->
-
-
-            <el-input size="mini" v-model.trim="editingRow[column.prop]"></el-input>
-          </template>
-          <template v-else>
-            {{scope.row[column.prop]}}
-          </template>
-        </template>
-      </el-table-column>
-
-
-      <el-table-column
-        label="操作"
-        width="100">
-        <template slot-scope="scope">
-
-          <template v-if="scope.row.isEditing">
-            <el-button @click="saveEdit(scope.$index)"
-                       type="text"
-                       size="small">
-              保存
-            </el-button>
-            <el-button @click="cancelEdit(scope.$index)" type="text" size="small">取消</el-button>
-          </template>
-          <template v-else>
-            <el-button @click="editRow(scope.$index)"
-                       type="text"
-                       size="small"
-                       :disabled="isOtherBtnDisabled">
-              编辑
-            </el-button>
-            <el-button @click="deleteRow(scope.$index, scope)"
-                       type="text"
-                       size="small"
-                       :disabled="isOtherBtnDisabled">
-              删除
-            </el-button>
-          </template>
-
-
-        </template>
-      </el-table-column>
-
-      <template slot="append">
-        <div class="append-row">
-          <el-button class="button"
-                     @click="insertRow"
-                     type="text"
-                     :disabled="isOtherBtnDisabled"
-                     icon="el-icon-plus">插入
-          </el-button>
-        </div>
-      </template>
-    </el-table>
-
-    <div>
-      <pre>{{tableData}}</pre>
-    </div>
+    <el-switch
+      @change="e => change(e, 'yes', 'no')"
+      v-model="value.value"
+      active-value="yes"
+      active-text="是"
+      inactive-text="否"
+      inactive-value="no">
+    </el-switch>
 
   </div>
 </template>
@@ -96,139 +16,80 @@
 <script>
 // TODO: 同时插入多个
 import Sortable from 'sortablejs'
+import qs from 'qs';
 
+const request = () => new Promise(((resolve, reject) => {
+  setTimeout(reject, 2000)
+}))
 
 export default {
   name: 'demo0',
   data() {
     return {
-      tableData: [],
-
-      handlerSelector: 'sortable-move-handler',
-
-      rowEditingState: '',
-
-      editingRow: {},
-
-      columns: [
-        { label: '名称', prop: 'name', rowId: 'rowId' },
-        { label: 'X', prop: 'x', rowId: 'rowId' },
-        { label: 'Y', prop: 'y', rowId: 'rowId' },
-        { label: 'Z', prop: 'z', rowId: 'rowId' },
-      ]
+      value: {
+        value: 'no'
+      },
+      loading: null,
     }
   },
 
   mounted() {
-    this.fetchData().then(v => {
-      this.tableData = this.initTableData(v);
-      console.log(this.tableData)
-    });
-
-
-    // 可拖动
-    this.$nextTick(() => {
-      const table = document.querySelector('.el-table__body-wrapper tbody');
-      const self = this;
-
-      Sortable.create(table, {
-        handle: `.${self.handlerSelector}`,
-        onEnd({ newIndex, oldIndex }) {
-          const targetRow = self.tableData.splice(oldIndex, 1)[0];
-          self.tableData.splice(newIndex, 0, targetRow)
-        }
-      })
-    })
+    const a = {a: [[1, 2, 3]]};
+    console.log(JSON.stringify(a));
+    console.log(qs.stringify({a: [[1, 2, 3], [5, 6, 6]]},  { encode: false, arrayFormat: 'repeat' }))
   },
 
   methods: {
-    indexMethod(index) {
-      if (index === 0) {
-        return '起点';
-      }
-      if (index === this.tableData.length - 1) {
-        return '终点';
-      }
-      return index + 1;
-    },
-    fetchData() {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve([])
-        }, 1000)
+    change(newState, activeValue, inactiveValue) {
+
+      this.$confirm('检测到未保存的内容，是否在离开页面前保存修改？', '确认信息', {
+        confirmButtonText: '保存',
+        cancelButtonText: '放弃修改',
+        closeOnPressEscape: false,
+        closeOnClickModal: false,
+        customClass: 'message-box',
+        beforeClose: (action, instance, done) => {
+          if (action === 'confirm') {
+            if(!this.loading) {
+              this.loading = this.$loading({
+                target: '.message-box',
+                lock: true,
+                text: 'Loading',
+              });
+              console.log(this.loading);
+            } else {
+              this.loading();
+            }
+
+            request().then(v => {
+
+              done();
+              this.$message({
+                message: '激活成功',
+                type: 'success',
+                duration: 500,
+              });
+
+            }).catch(() => {
+              this.$message({
+                message: '激活失败',
+                type: 'error',
+                duration: 100,
+              });
+            }).finally(() => {
+              this.loading.close();
+            })
+          } else {
+            done();
+          }
+        }
       }).then(v => {
-        let startPoint = {},
-          endPoint = {},
-          middlePoints = [];
 
-        if (v.length > 0) {
-          startPoint = { ...v.splice(0, 1)[0] };
-          endPoint = { ...v.splice(v.length - 1, 1)[0] };
-          middlePoints = [...v];
-        }
+      }).catch(() => {
 
-        return ({ startPoint, endPoint, middlePoints });
+        this.value.value = activeValue === newState ? inactiveValue : activeValue;
       })
-    },
-
-    // 数据初始化
-    initTableData({ startPoint, endPoint, middlePoints }) {
-      let tempStartPoint = {
-          rowId: 'start',
-          ...startPoint
-        },
-        tempEndPoint = { rowId: 'end', ...endPoint };
-
-      return [tempStartPoint, ...middlePoints, tempEndPoint].map((v, index) => (
-        {
-          name: v.name || '',
-          x: v.x || '',
-          y: v.y || '',
-          z: v.z || '',
-          rowId: v.rowId || index,
-          isEditing: false,
-        }
-      ));
-    },
-
-    deleteRow(index, scope) {
-      console.log(scope);
-      this.tableData.splice(index, 1)
-    },
-
-    insertRow() {
-      this.rowEditingState = 'insert';
-      this.tableData.splice(this.tableData.length - 2, 0, {
-        x: '',
-        y: '',
-        z: '',
-        name: '',
-        rowId: this.tableData.length - 2,
-        isEditing: true
-      });
-    },
-
-    editRow(index) {
-      this.rowEditingState = 'edit';
-      this.editingRow = Object.assign({}, this.tableData[index]);
-      this.$set(this.tableData[index], 'isEditing', true);
-    },
-
-    saveEdit(index) {
-      this.tableData[index] = {
-        ...this.editingRow,
-        isEditing: false,
-      };
-
-      this.rowEditingState = '';
-      this.editingRow = {};
-    },
-
-    cancelEdit(index) {
-      this.editingRow = {};
-      this.tableData[index].isEditing = false;
-      this.rowEditingState = '';
-    },
+    }
   },
 
   computed: {
--- src/components/demos/demo40.vue
+++ src/components/demos/demo40.vue
@@ -1,88 +1,89 @@
 <template>
   <div class="main">
-
-    <el-table :data="tableData" row-key="rowId" :row-class-name="setRowClassName" @row-dblclick="dbClickHandler">
-
-      <!-- 拖拽手柄 -->
-      <el-table-column width="50">
-        <template slot-scope="scope">
-          <el-button v-if="!isStartOrEndRow(scope.row.rowId)"
-                     :class="handlerSelector"
-                     icon="el-icon-rank"
-                     size="small"
-                     type="text">
-          </el-button>
-        </template>
-      </el-table-column>
-
-      <!-- 序号 -->
-      <el-table-column type="index" width="80" :index="indexMethod"></el-table-column>
-
-
-      <!-- 实际内容 -->
-      <el-table-column v-for="column in columns"
-                       :key="column.prop"
-                       :prop="column.prop"
-                       :label="column.label"
-                       :min-width="column.minWidth">
-        <template slot-scope="scope">
-          <template v-if="scope.row.isEditing">
-
-            <el-input size="mini" v-model.trim="editingRow[column.prop]"></el-input>
-          </template>
-          <template v-else>
-            {{scope.row[column.prop]}}
-          </template>
-        </template>
-      </el-table-column>
-
-
-      <!-- 操作 -->
-      <el-table-column label="操作" width="100">
-        <template slot-scope="scope">
-          <template v-if="scope.row.isEditing">
-            <el-button @click="saveEdit(scope.$index)"
-                       type="text"
-                       size="small"
-                       :disabled="isRowSaveBtnDisabled">
-              保存
-            </el-button>
-            <el-button @click="cancelEdit(scope.$index)" type="text" size="small">
-              取消
-            </el-button>
-          </template>
-          <template v-else>
-            <el-button @click="editRow(scope.$index)" type="text" size="small" :disabled="isRowEditing">
-              编辑
-            </el-button>
+    <h1>demo40 - 可拖拽表格</h1>
+    <div class="content-container">
+      <el-table :data="tableData"
+                row-key="rowId"
+                :row-class-name="setRowClassName"
+                border
+                @row-dblclick="dbClickHandler"
+                class="table">
+        <!-- 拖拽手柄 -->
+        <el-table-column width="50">
+          <template slot-scope="scope">
             <el-button v-if="!isStartOrEndRow(scope.row.rowId)"
-                       @click="deleteRow(scope.$index)"
-                       type="text"
+                       :class="handlerSelector"
+                       icon="el-icon-rank"
                        size="small"
-                       :disabled="isRowEditing">
-              删除
+                       type="text">
             </el-button>
           </template>
+        </el-table-column>
+
+        <!-- 序号 -->
+        <el-table-column type="index" width="80" :index="indexMethod"></el-table-column>
+
+        <!-- 实际内容 -->
+        <el-table-column v-for="column in columns"
+                         :key="column.prop"
+                         :prop="column.prop"
+                         :label="column.label"
+                         :min-width="column.minWidth">
+          <template slot-scope="scope">
+            <template v-if="scope.row.isEditing">
+              <el-input size="mini" v-model.trim="editingRow[column.prop]"></el-input>
+            </template>
+            <template v-else>
+              {{scope.row[column.prop]}}
+            </template>
+          </template>
+        </el-table-column>
+
+        <!-- 操作 -->
+        <el-table-column label="操作" width="100">
+          <template slot-scope="scope">
+            <template v-if="scope.row.isEditing">
+              <el-button @click="saveEdit(scope.$index)"
+                         type="text"
+                         size="small"
+                         :disabled="isRowSaveBtnDisabled">
+                保存
+              </el-button>
+              <el-button @click="cancelEdit(scope.$index)" type="text" size="small">
+                取消
+              </el-button>
+            </template>
+            <template v-else>
+              <el-button @click="editRow(scope.$index)" type="text" size="small" :disabled="isRowEditing">
+                编辑
+              </el-button>
+              <el-button v-if="!isStartOrEndRow(scope.row.rowId)"
+                         @click="deleteRow(scope.$index)"
+                         type="text"
+                         size="small"
+                         :disabled="isRowEditing">
+                删除
+              </el-button>
+            </template>
+          </template>
+        </el-table-column>
+
+        <template slot="append">
+          <div class="append-row">
+            <el-button class="button"
+                       @click="insertRow"
+                       type="text"
+                       :disabled="isRowEditing"
+                       icon="el-icon-plus">
+              插入
+            </el-button>
+          </div>
         </template>
-      </el-table-column>
-
-      <template slot="append">
-        <div class="append-row">
-          <el-button class="button"
-                     @click="insertRow"
-                     type="text"
-                     :disabled="isRowEditing"
-                     icon="el-icon-plus">
-            插入
-          </el-button>
-        </div>
-      </template>
-    </el-table>
-
-    <div>
-      <pre>{{tableData}}</pre>
+      </el-table>
+      <div class="code">
+        <pre>{{tableData}}</pre>
+      </div>
     </div>
-
   </div>
 </template>
 
@@ -96,7 +97,13 @@ export default {
   name: 'demo40',
   data() {
     return {
-      tableData: [],
+      tableData: [
+        {name: 'start', x: '0', y: '0', z: '0', rowId: 'start' },
+        {name: '一', x: '1', y: '1', z: '1', rowId: 'middle-1' },
+        {name: '二', x: '2', y: '2', z: '2', rowId: 'middle-2' },
+        {name: '三', x: '3', y: '3', z: '3', rowId: 'middle-3' },
+        {name: 'end', x: '100', y: '100', z: '100', rowId: 'end' },
+      ],
 
       handlerSelector: 'sortable-move-handler',
 
@@ -108,39 +115,15 @@ export default {
       },
 
       columns: [
-        {label: '名称', prop: 'name'},
-        {label: 'X', prop: 'x'},
-        {label: 'Y', prop: 'y'},
-        {label: 'Z', prop: 'z'},
+        { label: '名称', prop: 'name' },
+        { label: 'X', prop: 'x' },
+        { label: 'Y', prop: 'y' },
+        { label: 'Z', prop: 'z' },
       ]
     }
   },
 
   mounted() {
-    this.fetchData().then(v => {
-      this.tableData = this.initTableData(v);
-    });
-
-    const a1 = [0, 5, 0, 14];
-    const a2 = [1, 2, 10, 4];
-    const a3 = [0, 23, 13, 4];
-
-    let max = [a1, a2, a3].reduce((total, current) => {
-      current.forEach((num, day) => {
-        if (!total[day]) {
-          total[day] = {total: 0};
-        }
-        total[day].total += num;
-      });
-      return total;
-    }, []).sort((a, b) => b.total - a.total)[0].total;
-
-    max = 101
-    const digits = Math.pow(10, String(max).length);
-    const digital = (max / digits) < 0.5 ? 0.5 : 1;
-    console.log(digital * digits);
-
-
     // 可拖动
     this.$nextTick(() => {
       const table = document.querySelector('.el-table__body-wrapper tbody');
@@ -148,7 +131,8 @@ export default {
 
       Sortable.create(table, {
         handle: `.${self.handlerSelector}`,
-        onEnd({newIndex, oldIndex}) {
+        onEnd({ newIndex, oldIndex }) {
+
           const targetRow = self.tableData.splice(oldIndex, 1)[0];
           self.tableData.splice(newIndex, 0, targetRow);
         },
@@ -160,44 +144,6 @@ export default {
   },
 
   methods: {
-    // 获取数据
-    fetchData() {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve([1, 2, 3, 4, 5])
-        }, 1000)
-      }).then(v => {
-        let startPoint = {},
-          endPoint = {},
-          middlePoints = [];
-
-        if (v.length > 0) {
-          startPoint = {...v.splice(0, 1)[0]};
-          endPoint = {...v.splice(v.length - 1, 1)[0]};
-          middlePoints = [...v];
-        }
-
-        return ({startPoint, endPoint, middlePoints});
-      })
-    },
-
-    // 数据初始化
-    initTableData({startPoint, endPoint, middlePoints}) {
-      let tempStartPoint = {rowId: START_ROW_ID, ...startPoint},
-        tempEndPoint = {rowId: END_ROW_ID, ...endPoint};
-
-      return [tempStartPoint, ...middlePoints, tempEndPoint].map(v => (
-        {
-          name: v.name || '',
-          x: v.x || '',
-          y: v.y || '',
-          z: v.z || `middle-${this.uuRowId}`,
-          rowId: v.rowId || `middle-${this.uuRowId++}`,
-          isEditing: false,
-        }
-      ));
-    },
-
     // 设置行序号
     indexMethod(index) {
       if (index === 0) {
@@ -210,7 +156,7 @@ export default {
     },
 
     // 起始行设置类型，禁止其他行与起始行交换位置
-    setRowClassName({row}) {
+    setRowClassName({ row }) {
       if (this.isStartOrEndRow(row.rowId)) {
         return 'forbid-drag-row'
       }
@@ -228,7 +174,7 @@ export default {
         return;
       }
       const prop = column.property;
-      const isContentColumn = this.columns.map(({prop}) => prop).includes(prop);
+      const isContentColumn = this.columns.map(({ prop }) => prop).includes(prop);
 
       if (isContentColumn) {
         const index = this.tableData.findIndex(v => v.rowId === row.rowId);
@@ -288,8 +234,6 @@ export default {
         isEditing: false,
         isNew: false,
       };
-
-
     },
   },
 
@@ -300,7 +244,7 @@ export default {
 
     // 编辑行时，所有编辑项有值才可以保存
     isRowSaveBtnDisabled() {
-      const {rowId} = this.editingRow;
+      const { rowId } = this.editingRow;
       const validateProps = this.isStartOrEndRow(rowId) ? ['name', 'x', 'y'] : ['x', 'y'];
       return validateProps.some(prop => !String(this.editingRow[prop]));
     },
@@ -314,10 +258,29 @@ export default {
   .main {
     font-size: 16px;
     color: darkcyan;
+    h1 {
+      margin-bottom: 20px;
+    }
+    .content-container {
+      display: flex;
+      flex-wrap: nowrap;
+      align-items: center;
+      justify-content: center;
+    }
+    .table {
+      flex: 1 0 60%;
+      width: 60%;
+      margin: 0 20px;
+    }
+    .code {
+      flex: auto;
+      text-align: left;
+      border: 1px solid gainsboro;
+    }
   }
 
   pre {
-    text-align: left;
+
   }
 
   .button {
@@ -332,4 +295,6 @@ export default {
       background: #F2F7FF;
     }
   }
+
+
 </style>
